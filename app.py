3e30822d1d75308908e570de07b0e93e089d6acd
2022BCS0020-JAYANTH <kurivella22bcs20@iiitkottayam.ac.in>
2026-02-13 16:37:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import joblib
 import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
+import os
 
-# ------------------ LOAD MODEL ------------------
-model = joblib.load("model.pkl")
+MODEL_PATH = "model.pkl" 
+
+if not os.path.exists(MODEL_PATH):
+    MODEL_PATH = "app/artifacts/model.pkl"
+
+model = joblib.load(MODEL_PATH)
 
 app = FastAPI()
 
-# ------------------ INPUT SCHEMA ------------------
 class WineFeatures(BaseModel):
     fixed_acidity: float
     volatile_acidity: float
@@ -22,25 +27,24 @@ class WineFeatures(BaseModel):
     sulphates: float
     alcohol: float
 
-# ------------------ PREDICTION ENDPOINT ------------------
+@app.get("/")
+def home():
+    return {
+        "status": "Success",
+        "message": "Wine Quality Prediction API",
+        "developer": "K. Jayanth"
+    }
+
 @app.post("/predict")
 def predict(features: WineFeatures):
     X = np.array([[
-        features.fixed_acidity,
-        features.volatile_acidity,
-        features.citric_acid,
-        features.residual_sugar,
-        features.chlorides,
-        features.free_sulfur_dioxide,
-        features.total_sulfur_dioxide,
-        features.density,
-        features.pH,
-        features.sulphates,
-        features.alcohol
+        features.fixed_acidity, features.volatile_acidity, features.citric_acid,
+        features.residual_sugar, features.chlorides, features.free_sulfur_dioxide,
+        features.total_sulfur_dioxide, features.density, features.pH,
+        features.sulphates, features.alcohol
     ]])
-
+    
     prediction = model.predict(X)[0]
-
     wine_quality = int(round(prediction))
 
     return {
@@ -48,3 +52,6 @@ def predict(features: WineFeatures):
         "roll_no": "2022BCS0020",
         "wine_quality": wine_quality
     }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
